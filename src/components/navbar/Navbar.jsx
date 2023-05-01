b6b4624f95542956647336a908bfd6dbf5b44544
GAMA <abashia.nikoloza@gmail.com>
2023-05-01 21:34:52 +0400

    unilab-todo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,13 +2,14 @@ import "./navbar.scss";
 
 //Navbar
 const Navbar = () => {
-  const JoinedUser = JSON.parse(localStorage.getItem("joinedUser"));
+  const joinedUser = JSON.parse(localStorage.getItem("joinedUser"));
+
   return (
     <div className="navbar_container">
       <h1>TO DO</h1>
       <div className="navbar_right_side">
-        <div>{JoinedUser?.FirstName ? JoinedUser?.FirstName : "YourName"}</div>
-        <img src={JoinedUser?.Profile} alt="" />
+        <div>{joinedUser?.FirstName ? joinedUser?.FirstName : "YourName"}</div>
+        <img src={joinedUser?.Profile} alt="" />
       </div>
     </div>
   );
--- src/pages/sign/SignIn.jsx
+++ src/pages/sign/SignIn.jsx
@@ -8,6 +8,7 @@ import "./signIn.scss";
 const SignIn = () => {
   const [enteredName, setEnteredName] = useState("");
   const [userProfile, setUserProfile] = useState("");
+  const [errors, setError] = useState(false);
 
   const navigate = useNavigate();
 
@@ -30,45 +31,66 @@ const SignIn = () => {
     });
   };
 
-  function handleJoin() {
+  const handleJoin = () => {
     if (!userProfile || !enteredName) return;
     const enteredValues = {
       FirstName: enteredName,
       Profile: userProfile,
     };
+
     localStorage.setItem("joinedUser", JSON.stringify(enteredValues));
 
     navigate("/todo");
-  }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (enteredName.length == 0 || userProfile.length == 0) {
+      setError(true);
+    }
+  };
 
   return (
-    <div className="registrer_container">
-      <div className="registrer_box">
-        <h1>Get Started</h1>
-        <label htmlFor="fileInput">
-          add a photo
-          <div className="img_bubble">
-            <img src={userProfile || UserPhoto} alt="" />
-          </div>
-        </label>
-        <input
-          style={{ display: "none" }}
-          type="file"
-          onChange={(e) => {
-            uploadImage(e);
-          }}
-          id="fileInput"
-        />
-        <label>fill in you name</label>
-        <input
-          value={enteredName}
-          onChange={(e) => setEnteredName(e.target.value)}
-          type="text"
-          placeholder="your name"
-        />
-        <button onClick={handleJoin}>Sign In</button>
+    <form onSubmit={handleSubmit}>
+      <div className="registrer_container">
+        <div className="registrer_box">
+          <h1>Get Started</h1>
+          <label htmlFor="fileInput">
+            add a photo
+            <div className="img_bubble">
+              <img src={userProfile || UserPhoto} alt="" />
+            </div>
+          </label>
+          <input
+            style={{ display: "none" }}
+            type="file"
+            onChange={(e) => {
+              uploadImage(e);
+            }}
+            id="fileInput"
+          />
+          {errors && userProfile.length <= 0 ? (
+            <div className="error_messages">გთხოვთ აირჩიოთ ფოტო </div>
+          ) : (
+            ""
+          )}
+          <label>fill in you name</label>
+          <input
+            value={enteredName}
+            onChange={(e) => setEnteredName(e.target.value)}
+            type="text"
+            placeholder="your name"
+          />
+          {errors && enteredName.length <= 0 ? (
+            <div className="error_messages">გთხოვთ შეავსოთ </div>
+          ) : (
+            ""
+          )}
+          <button onClick={handleJoin}>Sign In</button>
+        </div>
       </div>
-    </div>
+    </form>
   );
 };
 
